21161b913e67ae16549d2bb1fb9c7beb20c74388
darqwski <dariusz.cabala.pl@gmail.com>
2022-05-14 14:45:52 +0200

    [CreditCardManager] - Better showing of card

--- app/src/main/java/dariusz/cabala/cards/project/adapters/CreditCardAdapter.kt
+++ app/src/main/java/dariusz/cabala/cards/project/adapters/CreditCardAdapter.kt
@@ -3,6 +3,7 @@ package dariusz.cabala.cards.project.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import dariusz.cabala.cards.project.R
@@ -11,16 +12,23 @@ import dariusz.cabala.cards.project.model.CreditCard
 class CreditCardAdapter (private val dataSet: Array<CreditCard>) :
     RecyclerView.Adapter<CreditCardAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
+        val cardName: TextView
+        val cardNumber: TextView
+        val cardDate: TextView
+        val cardProvider: TextView
+        val editButton: Button
+        val toggleButton: Button
+        val deleteButton: Button
 
         init {
-            // Define click listener for the ViewHolder's View.
-            textView = view.findViewById(R.id.creditCardName)
+            cardName = view.findViewById(R.id.creditCardName)
+            cardNumber = view.findViewById(R.id.creditCardRowCardNumber)
+            cardDate = view.findViewById(R.id.creditCardRowDate)
+            cardProvider = view.findViewById(R.id.creditCardRowProviderName)
+            editButton = view.findViewById(R.id.editCardButton)
+            toggleButton = view.findViewById(R.id.showNumberButton)
+            deleteButton = view.findViewById(R.id.removeCardButton)
         }
     }
 
@@ -33,12 +41,47 @@ class CreditCardAdapter (private val dataSet: Array<CreditCard>) :
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    fun maskCardCharacter(character: Char): Char{
+        if(character == '0' || character == '1' || character == '2' || character == '3' || character == '4' ||
+            character == '5' || character == '6' || character == '7' || character == '8' || character == '9' )
+            return '*'
+
+        return character
+    }
+
+    fun maskCardNumber(cardNumber: String): String {
+        var buildedString = ""
+        for (i in cardNumber.indices) {
+            if(i >= cardNumber.length - 4){
+                buildedString += cardNumber[i]
+            } else {
+                buildedString += maskCardCharacter(cardNumber[i])
+            }
+        }
+        return buildedString;
+    }
+
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.textView.text = dataSet[position].cardName
+        val creditCard = dataSet[position]
+        viewHolder.cardName.text = creditCard.cardName
+        viewHolder.cardNumber.text = maskCardNumber(creditCard.cardNumber)
+        viewHolder.cardProvider.text = creditCard.cardProvider
+        viewHolder.cardDate.text = creditCard.mothYear
+
+        viewHolder.toggleButton.setOnClickListener {
+            val asButton: Button = it as Button
+
+            if (asButton.text.equals("Pokaż")) {
+                asButton.text = "Ukryj"
+                viewHolder.cardNumber.text = creditCard.cardNumber
+
+            } else {
+                asButton.text = "Pokaż"
+                viewHolder.cardNumber.text = maskCardNumber(creditCard.cardNumber)
+
+            }
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
