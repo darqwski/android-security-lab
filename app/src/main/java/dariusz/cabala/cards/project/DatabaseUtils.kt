56a66d78675b92fdc35d9dfcd11a76410be9f44a
darqwski <dariusz.cabala.pl@gmail.com>
2022-06-02 18:58:15 +0200

    [CreditCardManager] - DB Encryption

--- app/src/main/java/dariusz/cabala/cards/project/DatabaseUtils.kt
+++ app/src/main/java/dariusz/cabala/cards/project/DatabaseUtils.kt
@@ -2,6 +2,9 @@ package dariusz.cabala.cards.project
 
 import android.content.Context
 import androidx.room.Room
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import java.util.*
 
 class DatabaseUtils {
 
@@ -9,10 +12,17 @@ class DatabaseUtils {
         lateinit var db: CreditCardDatabase
 
         fun initDatabase(applicationContext: Context) {
-            db = Room.databaseBuilder(
+            val builder = Room.databaseBuilder(
                 applicationContext,
-                CreditCardDatabase::class.java, "database-name"
-            ).build()
+                CreditCardDatabase::class.java, "encypted"
+            )
+            val passphrase = SQLiteDatabase.getBytes(
+                Base64.getDecoder().decode("aGFzbG9fel9kb2x1X3BsZWNvdw==").toString().toCharArray()
+            )
+            val factory = SupportFactory(passphrase)
+            builder.openHelperFactory(factory)
+
+            db = builder.build()
         }
     }
 }
